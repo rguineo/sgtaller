c9e43111e217c9f7232008b9a63bbf412aa3a32b
rguineo <rguineoa@gmail.com>
2018-08-08 01:49:39 -0400

    clase conectar

--- model/conexion.php
+++ model/conexion.php
@@ -1,9 +1,16 @@
-<?php
+<?php 
 
-class conexion(){
+class Conexion {
 
-    
+	public function Conectar () {
+        $basedato = "tienda2";
+
+		$link = new PDO("mysql:host=localhost;dbname=$basedato", 
+			"root",
+			"",
+			array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+		);
+		return $link;
+	}
 }
-
-
 ?>
