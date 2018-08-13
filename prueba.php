7104368f1bac9db1bb3084e2afe7538333128722
rguineo <rguineoa@gmail.com>
2018-08-13 00:10:49 -0300

    Actualizando participantes

--- prueba.php
+++ prueba.php
@@ -1,7 +1,8 @@
 <?php
 
     echo 'Con el Git configurado';
-    echo 'Roberto Guineo A';
-    echo 'Technosoft ';
-
+        echo 'Roberto Guineo A';
+        echo 'Technosoft ';
+    echo 'Con el Git NO configurado';
+        echo 'Tito Caceres';
 ?>
