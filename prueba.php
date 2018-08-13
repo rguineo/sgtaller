c76aca74192fd845d5132911fc09745508adfd97
hugoseledon <hugoseledon@gmail.com>
2018-08-13 00:19:42 -0300

    prueba3

--- prueba.php
+++ prueba.php
@@ -4,6 +4,7 @@
         echo 'Roberto Guineo A';
         echo 'Technosoft ';
         echo 'hola soy shamid';
+        echo 'juguemos pubg???';
 
     echo 'COn el Git No configurado';
         echo 'Tito Caceres';
