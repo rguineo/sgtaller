a544f219eeb03057ef5e2ef7d961ab40ff8359d3
huajinfu <technosoft.eirl@gmail.com>
2018-08-13 00:25:19 -0300

    cambio

--- prueba.php
+++ prueba.php
@@ -1,15 +1,8 @@
 <?php
 
     echo 'Con el Git configurado';
-<<<<<<< HEAD
-    echo 'Roberto Guineo A';
-    echo 'Technosoft ';
-    echo 'hola soy shamid';
-
-=======
         echo 'Roberto Guineo A';
         echo 'Technosoft ';
-    echo 'Con el Git NO configurado';
-        echo 'Tito Caceres';
->>>>>>> 4e19930f313599ef9c9de0f5b1157ef9469be74b
+        echo 'hola soy shamid';
+
 ?>
