2fa27709b620badad8e6ae7e3bacbddada624d98
rguineo <rguineoa@gmail.com>
2018-08-13 00:16:31 -0300

    Actualizando participantes

--- prueba.php
+++ prueba.php
@@ -1,15 +1,11 @@
 <?php
 
     echo 'Con el Git configurado';
-<<<<<<< HEAD
-    echo 'Roberto Guineo A';
-    echo 'Technosoft ';
-    echo 'hola soy shamid';
-
-=======
         echo 'Roberto Guineo A';
         echo 'Technosoft ';
-    echo 'Con el Git NO configurado';
+        echo 'hola soy shamid';
+
+    echo 'COn el Git No configurado';
         echo 'Tito Caceres';
->>>>>>> 4e19930f313599ef9c9de0f5b1157ef9469be74b
+        echo 'Hugo Seledon';
 ?>
