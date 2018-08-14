d6e1369a6eefd9d0e0ccb3c699da428c47ad44e2
cacerestm <caceres_tm@hotmail.com>
2018-08-14 13:49:32 -0300

    nueva actualizacion

--- prueba.php
+++ prueba.php
@@ -5,7 +5,5 @@
         echo 'Technosoft ';
         echo 'hola soy shamid';
         echo 'juguemos pubg???';
-
-    echo 'COn el Git No configurado';
-        echo 'Tito Caceres';
+        echo 'hola soy #Chocolatito!!!!!';
 ?>
