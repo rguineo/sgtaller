d80f24251ae03e6c7e9c65d66119696567a467ad
cacerestm <caceres_tm@hotmail.com>
2018-08-14 15:04:22 -0300

    show update

--- prueba.php
+++ prueba.php
@@ -4,7 +4,6 @@
         echo 'Roberto Guineo A';
         echo 'Technosoft ';
         echo 'hola soy shamid';
-        echo 'juguemos pubg???';
         echo 'hola soy #Chocolatito!!!!!';
         echo 'funciona esta wea????';
 ?>
