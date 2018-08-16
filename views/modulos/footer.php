91f4be0ac0a32744f6c8643946acdf26f24d0695
rguineo <rguineoa@gmail.com>
2018-08-16 20:19:47 -0300

    Footer cambiado

--- views/modulos/footer.php
+++ views/modulos/footer.php
@@ -1,7 +1,7 @@
  <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Backend del ecommerce
+      Sistema de Gestion Taller
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2018 <a href="">Duotek Services</a>.</strong> Todos los derechos reservados.
