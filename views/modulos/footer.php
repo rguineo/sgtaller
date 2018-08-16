0fadddbd3af88740979033bdc527517eb0f01f0f
rguineo <rguineoa@gmail.com>
2018-08-16 20:21:26 -0300

    Footer de nuevo

--- views/modulos/footer.php
+++ views/modulos/footer.php
@@ -1,7 +1,7 @@
  <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Sistema de Gestion Taller
+      Sistema de Gestión Taller
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2018 <a href="">Duotek Services</a>.</strong> Todos los derechos reservados.
