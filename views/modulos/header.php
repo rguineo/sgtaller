9b41e6a161cefd6cc7324755f84e90ae17be370d
hugoseledon <hugoseledon@gmail.com>
2018-08-16 20:40:45 -0300

    pez1

--- views/modulos/header.php
+++ views/modulos/header.php
@@ -5,7 +5,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>S</b>GT</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>Duotek</b>SGT</span>
+      <img src="views/dist/img/DT_Blanco.png" alt="" width="150">
     </a>
 
     <!-- Header Navbar -->
