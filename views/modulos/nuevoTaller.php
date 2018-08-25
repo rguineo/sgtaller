6704f9951251703f4d0ba79f21bb40ea9847b482
cacerestm <caceres_tm@hotmail.com>
2018-08-25 01:03:25 -0300

    Actualizacion Formulario Taller

--- views/modulos/nuevoTaller.php
+++ views/modulos/nuevoTaller.php
@@ -22,21 +22,48 @@
             <input type="text" class="form-control" id="inputDTALLER" placeholder="Ingrese la direccion del Taller" required>
           </div>
           <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputComuna">Seleccione Comuna</label>
-            <input type="text" class="form-control" id="inputComuna" placeholder="Ingrese Nombre de Nuevo Taller" required>
+            <div class="form-group col-md-4">
+              <label for="inputComuna">Region</label>
+                <select class="form-control">
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Seleccione Region</font></font></option>
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Region 1</font></font></option>
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Region 2</font></font></option>
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Region 3</font></font></option>
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Region 4</font></font></option>
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Region 5</font></font></option>
+                  </select>
+              </div>
+          <div class="form-group col-md-4">
+            <label for="inputRegion">Comuna</label>
+              <select class="form-control">
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Seleccione Comuna</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Comuna 1</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Comuna 2</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Comuna 3</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Comuna 4</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Comuna 5</font></font></option>
+              </select>
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputRegion">Ciudad</label>
+                <select class="form-control">
+                    <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Seleccione Ciudad</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ciudad 1</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ciudad 2</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ciudad 3</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ciudad 4</font></font></option>
+                      <option><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ciudad 5</font></font></option>
+                </select>
+            </div>
+            <div class="form-group col-md-12">
+            <label for="inputDTALLER">URL Ubicacion Taller</label>
+            <input type="text" class="form-control" id="inputURLTaller" placeholder="Ingrese la URL de Ubicacion del Taller" required>
           </div>
-          <div class="form-group col-md-6">
-            <label for="inputRegion">Seleccione Region</label>
-            <input type="text" class="form-control" id="inputRegion" placeholder="Ingrese la direccion del Taller" required>
+            <div class="form-group col-md-12">
+            <br>
+            <button type="submit" class="btn btn-primary">Guardar</button>
           </div>
-        </div>
-        </div>
-        <div class="form-group col-md-12">
-          <br>
-          <button type="submit" class="btn btn-primary">Guardar</button>
-        </div>
-      </form>
-    </section>
+        </form>
+      </section>
     <!-- /.content -->
-</div>
+  </div>
