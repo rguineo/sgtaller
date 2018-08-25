4b39cc5af55da818428ab63ef9cec9963c97a646
hugoseledon <hugoseledon@gmail.com>
2018-08-25 00:52:54 -0300

    form

--- views/modulos/modificarEquipo.php
+++ views/modulos/modificarEquipo.php
@@ -1,20 +1,146 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1>
-        Gestor de Equipos
-        
-      </h1>
+      <h1 class="tituloModificarEquipo">Gestor de Equipos</h1>
+      <br>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-        <li class="active">Here</li>
+        <li><a href="home"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Administrar Equipos</li>
       </ol>
     </section>
 
     <!-- Main content -->
-    <section class="content container-fluid">
+    <div class="box">
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>#</th>
+                  <th>N° Serie</th>
+                  <th>Nombre Equipo</th>
+                  <th>Marca Equipo</th>
+                  <th>Modelo Equipo</th>
+                  <th>Empresa</th>
+                  <th>Fecha Ingreso</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>0001
+                  </td>
+                  <td>Equipo 1</td>
+                  <td>marca 1</td>
+                  <td>X</td>
+                  <td>Empresa 1</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>0002
+                  </td>
+                  <td>Equipo 2</td>
+                  <td>marca 2</td>
+                  <td>C</td>
+                  <td>Empresa 2</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>0003
+                  </td>
+                  <td>Equipo 3</td>
+                  <td>marca 3</td>
+                  <td>A</td>
+                  <td>Empresa 3</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>0004
+                  </td>
+                  <td>Equipo 4</td>
+                  <td>marca 4</td>
+                  <td>A</td>
+                  <td>Empresa 4</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>0005</td>
+                  <td>Equipo 5</td>
+                  <td>marca 5</td>
+                  <td>A</td>
+                  <td>Empresa 5</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>0006</td>
+                  <td>Equipo 6</td>
+                  <td>marca 6</td>
+                  <td>A</td>
+                  <td>Empresa 6</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>7</td>
+                  <td>0007</td>
+                  <td>Equipo 7</td>
+                  <td>marca 7</td>
+                  <td>A</td>
+                  <td>Empresa 7</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>8</td>
+                  <td>0008</td>
+                  <td>Equipo 8</td>
+                  <td>marca 8</td>
+                  <td>A</td>
+                  <td>Empresa 8</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>9</td>
+                  <td>0009</td>
+                  <td>Equipo 9</td>
+                  <td>marca 9</td>
+                  <td>A</td>
+                  <td>Empresa 9</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                <tr>
+                  <td>10</td>
+                  <td>00010</td>
+                  <td>Equipo 10</td>
+                  <td>marca 10</td>
+                  <td>A</td>
+                  <td>Empresa 10</td>
+                  <td>dd-mm-aaaa</td>
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+      <!-- <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">N° Serie</th>
+            <th scope="col">Nombre Equipo</th>
+            <th scope="col">Marca Equipo</th>
+            <th scope="col">Modelo Equipo</th>
+            <th scope="col">Empresa</th>
+            <th scope="col">Fecha Ingreso</th>
+          </tr>
+        </thead>
+        <tbody>
 
-      <h1>Aquí se modifican equipos</h1>
+
+        </tbody>
+      </table> -->
 
     </section>
     <!-- /.content -->
