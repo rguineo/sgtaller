d37ac5230e2132726c23d67ef3cf456f5754f7fb
hugoseledon <hugoseledon@gmail.com>
2018-09-01 01:50:25 -0300

    a

--- views/modulos/modificarEquipo.php
+++ views/modulos/modificarEquipo.php
@@ -1,147 +1,81 @@
+<?php 
+
+ $todosClientes = new ctrCliente(); 
+ $respuesta =  $todosClientes->ctrTodosClientes();
+
+?>
+
+
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <h1 class="tituloModificarEquipo">Gestor de Equipos</h1>
-      <br>
+      <h1>Equipos     </h1>
       <ol class="breadcrumb">
         <li><a href="home"><i class="fa fa-dashboard"></i> Inicio</a></li>
-        <li class="active">Administrar Equipos</li>
+        <li class="active">TodosEquipos</li>
       </ol>
     </section>
 
     <!-- Main content -->
-    <div class="box">
-            <div class="box-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>#</th>
-                  <th>N° Serie</th>
-                  <th>Nombre Equipo</th>
-                  <th>Marca Equipo</th>
-                  <th>Modelo Equipo</th>
-                  <th>Empresa</th>
-                  <th>Fecha Ingreso</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>0001
-                  </td>
-                  <td>Equipo 1</td>
-                  <td>marca 1</td>
-                  <td>X</td>
-                  <td>Empresa 1</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>0002
-                  </td>
-                  <td>Equipo 2</td>
-                  <td>marca 2</td>
-                  <td>C</td>
-                  <td>Empresa 2</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>0003
-                  </td>
-                  <td>Equipo 3</td>
-                  <td>marca 3</td>
-                  <td>A</td>
-                  <td>Empresa 3</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>4</td>
-                  <td>0004
-                  </td>
-                  <td>Equipo 4</td>
-                  <td>marca 4</td>
-                  <td>A</td>
-                  <td>Empresa 4</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>5</td>
-                  <td>0005</td>
-                  <td>Equipo 5</td>
-                  <td>marca 5</td>
-                  <td>A</td>
-                  <td>Empresa 5</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>6</td>
-                  <td>0006</td>
-                  <td>Equipo 6</td>
-                  <td>marca 6</td>
-                  <td>A</td>
-                  <td>Empresa 6</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>7</td>
-                  <td>0007</td>
-                  <td>Equipo 7</td>
-                  <td>marca 7</td>
-                  <td>A</td>
-                  <td>Empresa 7</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>8</td>
-                  <td>0008</td>
-                  <td>Equipo 8</td>
-                  <td>marca 8</td>
-                  <td>A</td>
-                  <td>Empresa 8</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>9</td>
-                  <td>0009</td>
-                  <td>Equipo 9</td>
-                  <td>marca 9</td>
-                  <td>A</td>
-                  <td>Empresa 9</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                <tr>
-                  <td>10</td>
-                  <td>00010</td>
-                  <td>Equipo 10</td>
-                  <td>marca 10</td>
-                  <td>A</td>
-                  <td>Empresa 10</td>
-                  <td>dd-mm-aaaa</td>
-                </tr>
-                </tfoot>
-              </table>
+    <section class="content container-fluid">
+
+    <br>
+      <div id='page-wrapper'>
+        <div class='container-fluid'>
+            <div class='row'>
+                <div class='col-lg-offset-1 col-lg-10'>
+                    <div class='table-responsive table_productos'>
+                        <table class='table table-striped table-bordered table-hover tabla-usuarios' id='dataTables-example'>
+
+                          <thead style='text-align: center; background: #eaeaea;'>
+                              <tr>
+                                  <th style='text-align: center;'> N° Serie</th>
+                                  <th style='text-align: center;'> Marca Equipo</th>
+                                  <th style='text-align: center;'> Modelo Equipo </th>	
+                                  <th style='text-align: center;'> Empresa </th>
+                                  <th style='text-align: center;'> Fecha Ingreso </th>
+                                  <th style='text-aling: center;'> Nombre Equipo </th>
+                                  <th style='text-align: center;'> Accion </th>
+                              </tr>
+                          </thead>
+
+                          <tbody>
+                          <?php 
+                           
+                           foreach ($respuesta as $key => $value) {
+                            echo "<tr>";
+                              echo "<td>".$value['rut']."</td>";
+                              echo "<td>".$value['razon_social']."</td>";
+                              echo "<td>".$value['giro']."</td>";
+                              echo "<td>".$value['direccion']."</td>"; 
+                              echo "<td>"; 
+                              echo "<center>";
+                                echo "<a href='#' title='Editar'><i class='fa fa-edit fa-2x'></i></a>";
+                                echo "<a href='#' data-toggle='modal' data-target='#delUsrModal' title='Eliminar'>
+                                <i class='fa fa-remove fa-2x'style='color: red;'></i></a>";
+                              echo "</center>";
+                              echo "</td>";
+                            echo "</tr>";
+                            }
+                          ?> 
+
+
+                            </tbody>
+                      </table>
+                  </div>
             </div>
-            <!-- /.box-body -->
           </div>
-      <!-- <table class="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">N° Serie</th>
-            <th scope="col">Nombre Equipo</th>
-            <th scope="col">Marca Equipo</th>
-            <th scope="col">Modelo Equipo</th>
-            <th scope="col">Empresa</th>
-            <th scope="col">Fecha Ingreso</th>
-          </tr>
-        </thead>
-        <tbody>
-
-
-        </tbody>
-      </table> -->
-
+        </div>
+      </div>
     </section>
     <!-- /.content -->
   </div>
+
+
+<th>#</th>
+<th>N° Serie</th>
+<th>Marca Equipo</th>
+<th>Modelo Equipo</th>
+<th>Empresa</th>
+<th>Fecha Ingreso</th>
+<th>Nombre Equipo</th>
