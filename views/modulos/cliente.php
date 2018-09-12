739aebf8b67c7eac5c5d923a2d66ca434dbd15fb
rguineo <rguineoa@gmail.com>
2018-09-12 20:18:38 -0300

    clientes ok

--- views/modulos/cliente.php
+++ views/modulos/cliente.php
@@ -18,7 +18,8 @@
 
     <!-- Main content -->
     <section class="content container-fluid">
-    <button type="button" class="btn bg-purple margin" data-toggle="modal" data-target="#modal-nuevo-cliente"><i class="fa fa-user-plus"></i>  Agregar Cliente</button>
+    <button type="button" class="btn bg-purple margin" data-toggle="modal" data-target="#modal-nuevo-cliente">
+    <i class="fa fa-user-plus"></i>  Agregar Cliente</button>
     <br><br>
       <div id='page-wrapper'>
         <div class='container-fluid'>
